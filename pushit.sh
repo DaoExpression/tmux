48e54b4c1c361110ec5b0740c4c89efe6f135d9b
om <ofm@autistici.org>
2023-10-20 16:33:42 +0100

    Make it Right for a better Ride

---
+++ pushit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# IF you are using another domain to push your code... 
+
+pkill -x ssh-agent 
+sleep 1
+echo "ssh-agents killed"
+eval $(ssh-agent)
+sleep 1 
+echo "ssh-agent inicialized"
+ssh-add ~/.ssh/omrsa
+sleep 1
+echo "ssh-key-id added"
+git add . 
+echo "all changes added to commit state"
+git commit -m "Make it Right for a better Ride"
+git push --set-upstream git@github.com:daoexpression/tmux main
+echo "Push completed"
+sleep 3
